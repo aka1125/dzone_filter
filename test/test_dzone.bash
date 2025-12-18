7a85d21f70788767b26e38ca3d38d0b27ca0e248
aka1125 <akayuusaku91@gmail.com>
2025-12-18 20:36:07 +0900

    change testbash

--- test/test_dzone.bash
+++ test/test_dzone.bash
@@ -9,7 +9,11 @@ cd $dir/ros2_ws
 colcon build --packages-select dzone_filter > /dev/null
 source install/setup.bash
 
-timeout 15 bash -c "source install/setup.bash && ros2 launch dzone_filter filter_launch.py" > /tmp/dzone_test.log 2>&1
+timeout 20 bash -c "
+    source /opt/ros/humble/setup.bash && 
+    source install/setup.bash && 
+    ros2 launch dzone_filter filter_launch.py
+" > /tmp/dzone_test.log 2>&1
 
 if grep -q "0.15" /tmp/dzone_test.log; then
 	exit 0
